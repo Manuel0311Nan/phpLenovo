a4ba7d9b407f4c89113be03a85307cedfd107e38
Manuel Alcázar <manuzz3112@gmail.com>
2022-08-03 12:19:13 +0200

    Igualo BBDD a la de Azken

--- registro.php
+++ registro.php
@@ -4,12 +4,16 @@ $conexion = connectServer(SERVER, USER, PASS, DATABASE);
 
 if(isset($_POST['register'])){
     if(strlen($_POST['nombre']) >=1 && strlen($_POST['email']) >= 1){
-        $nombre = trim($_POST['nombre']);
-        $email = trim($_POST['email']);
+        $nombre =ucfirst(trim($_POST['nombre'])) ;
+        $email =  trim($_POST['email']);
         $sentencia = $conexion->prepare("INSERT INTO usuarios(Nombre, Email) VALUES (:name, :email)");
         $sentencia ->bindParam(':name', $nombre);
         $sentencia ->bindParam(':email', $email);
         $sentencia->execute();
+        echo'<script type="text/javascript">
+         alert("Gracias, nos comunicaremos contigo muy pronto");
+        window.location.href="index.php";
+        </script>';
         } else{
             ?>
             <h3>Complete este campo</h3>
